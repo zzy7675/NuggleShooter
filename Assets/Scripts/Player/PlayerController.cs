ff0d96f9e3eb5a2b41734a4c41e905265dff0551
zzy7675 <1044187675@qq.com>
2026-09-15 07:56:05 +0800

    player movement polish
    1. add extra gravity when the player in the air exceeds an amount of time to quickly get back to the ground

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector2 _groundCheck;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _jumpStrength = 7f;
+    [SerializeField] private float _extraGravity = 700f;
+    [SerializeField] private float _gravityDelay = .2f;
+
+    private float _timeInAir;
 
     private PlayerInput _playerInput;
     private FrameInput _frameInput;
@@ -33,6 +37,12 @@ public class PlayerController : MonoBehaviour
         Movement();
         Jump();
         HandleSpriteFlip();
+        GravityDelay();
+    }
+
+    private void FixedUpdate()
+    {
+        ExtraGravity();
     }
 
     public bool IsFacingRight()
@@ -50,6 +60,25 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(_feetTransform.position, _groundCheck);
+    }
+
+    private void GravityDelay()
+    {
+        if (!CheckGrounded())
+        {
+            _timeInAir += Time.deltaTime;
+        } else
+        {
+            _timeInAir = 0;
+        }
+    }
+
+    private void ExtraGravity()
+    {
+        if (_timeInAir > _gravityDelay)
+        {
+            _rigidBody.AddForce(new Vector2(0f, -_extraGravity * Time.deltaTime));
+        }
     }
 
     private void GatherInput()
