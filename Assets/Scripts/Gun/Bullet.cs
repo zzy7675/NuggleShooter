54fe8c9ac16188f48d5408ffe751621ebbe3d0bd
zzy7675 <1044187675@qq.com>
2026-09-14 21:47:51 +0800

    weapon improvement
    1. implement bullet cooldown
    2. utilize observer pattern
    3. implement screen shake
    4. apply gun aim, set bullet direction towards mouse positions

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -10,20 +10,26 @@ public class Bullet : MonoBehaviour
     private Vector2 _fireDirection;
 
     private Rigidbody2D _rigidBody;
+    private Gun _gun;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    private void Start() {
-        if (PlayerController.Instance.IsFacingRight()) {
-            _fireDirection = Vector2.right;
-        } else {
-            _fireDirection = Vector2.left;
-        }
+    // private void Start() {
+    //     if (PlayerController.Instance.IsFacingRight()) {
+    //         _fireDirection = Vector2.right;
+    //     } else {
+    //         _fireDirection = Vector2.left;
+    //     }
+    // }
+    public void Init(Gun gun, Vector2 bulletSpawnPosition, Vector2 mousePosition)
+    {
+        _gun = gun;
+        transform.position = bulletSpawnPosition;
+        _fireDirection = (mousePosition - bulletSpawnPosition).normalized;
     }
-
     private void FixedUpdate()
     {
         _rigidBody.linearVelocity = _fireDirection * _moveSpeed;
@@ -32,6 +38,6 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         Health health = other.gameObject.GetComponent<Health>();
         health?.TakeDamage(_damageAmount);
-        Destroy(this.gameObject);
+        _gun.ReleaseBulletFromPool(this);
     }
 }
--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,28 +1,118 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Gun : MonoBehaviour
 {
-    public Transform BulletSpawnPoint => _bulletSpawnPoint;
+    public static Action OnShoot;
 
+    private ObjectPool<Bullet> _bulletPool;
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
-    
+    [SerializeField] private float _gunFireCD = 0.5f;
+
+    private static readonly int FIRE_HASH = Animator.StringToHash("Fire");
+    private float _lastFireTime = 0f;
+    private Vector2 _mousePosition;
+
+    private Animator _animator;
+    private CinemachineImpulseSource _impulseSource;
+
+    private void Awake()
+    {
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        CreateBulletPool();
+    }
+
     private void Update()
     {
         Shoot();
+        RotateGun();
+    }
+
+    private void OnEnable() {
+        OnShoot += ShootProjectile;
+        OnShoot += ResetLastFireTime;
+        OnShoot += FireAnimation;
+        OnShoot += GunScreenShake;
+    }
+
+    private void OnDisable() {
+        OnShoot -= ShootProjectile;
+        OnShoot -= ResetLastFireTime;
+        OnShoot -= FireAnimation;
+        OnShoot -= GunScreenShake;
+    }
+
+    public void ReleaseBulletFromPool(Bullet bullet)
+    {
+        _bulletPool.Release(bullet);
+    }
+
+    private void CreateBulletPool()
+    {
+        _bulletPool = new ObjectPool<Bullet>(
+            () =>
+            {
+                return Instantiate(_bulletPrefab);
+            },
+            bullet =>
+            {
+                bullet.gameObject.SetActive(true);
+            },
+            bullet =>
+            {
+                bullet.gameObject.SetActive(false);
+            },
+            bullet =>
+            {
+                Destroy(bullet);
+            }
+        );
     }
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            ShootProjectile();
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime) {
+            OnShoot?.Invoke();
         }
+    }
+
+    private void ResetLastFireTime()
+    {
+        _lastFireTime = Time.time + _gunFireCD;
     }
 
     private void ShootProjectile()
     {
-        Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+        Bullet newBullet = _bulletPool.Get();
+        newBullet.Init(this, (Vector2)_bulletSpawnPoint.position, _mousePosition);
+    }
+
+    private void FireAnimation()
+    {
+        _animator.Play(FIRE_HASH, 0, 0f);
+    }
+
+    private void GunScreenShake()
+    {
+        _impulseSource.GenerateImpulse();
+    }
+
+    private void RotateGun()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePosition);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
