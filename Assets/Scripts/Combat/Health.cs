a70d3cc2c2a2737032b994654f2a578a646d4a74
zzy7675 <1044187675@qq.com>
2026-09-15 15:18:13 +0800

    implement death VFX

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,15 +1,31 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Health : MonoBehaviour
 {
+    public Action OnDeath;
+    [SerializeField] private GameObject _splatterPrefab;
+    [SerializeField] private GameObject _deathVFX;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
 
     private void Start() {
         ResetHealth();
+    }
+
+    private void OnEnable()
+    {
+        OnDeath += SpawnDeathSplatterPrefab;
+        OnDeath += SpawnDeathVFX;
+    }
+
+    private void OnDisable()
+    {
+        OnDeath -= SpawnDeathSplatterPrefab;
+        OnDeath -= SpawnDeathVFX;
     }
 
     public void ResetHealth() {
@@ -20,7 +36,26 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
+            OnDeath?.Invoke();
             Destroy(gameObject);
         }
+    }
+
+    private void SpawnDeathSplatterPrefab()
+    {
+        GameObject newSplatterPrefab = Instantiate(_splatterPrefab, transform.position, transform.rotation);
+        SpriteRenderer deathSplatterSpriteRenderer = newSplatterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        Color currentColor = colorChanger.DefaultColor;
+        deathSplatterSpriteRenderer.color = currentColor;
+    }
+
+    private void SpawnDeathVFX()
+    {
+        GameObject deathVFX = Instantiate(_deathVFX, transform.position, transform.rotation);
+        ParticleSystem.MainModule ps = deathVFX.GetComponent<ParticleSystem>().main;
+        ColorChanger colorChanger = GetComponent<ColorChanger>();
+        Color currentColor = colorChanger.DefaultColor;
+        ps.startColor = currentColor;
     }
 }
